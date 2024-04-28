4e023b6ffccf31040da24e61b67ee8b47d3d2d7f
Irakli <112602982+IkaMastera@users.noreply.github.com>
2024-04-28 12:16:37 +0400

    add: js code for handling API

---
+++ script.js
@@ -0,0 +1,57 @@
+const container = document.querySelector(".container");
+const optionsContainer = document.querySelector(".options-container");
+const country = "ge";
+const options = [
+  "general",
+  "entertainment",
+  "health",
+  "science",
+  "sports",
+  "technology",
+];
+
+let requestURL;
+
+const generateUI = (articles) => {
+  for (let item of articles) {
+    let card = document.createElement("div");
+    card.classList.add("news-card");
+    card.innerHTML = `
+    <div class="news-image-container">
+       <img src="${item.urlToImage || "./newspaper.jpg"}" alt="" />
+    <div>
+    <div class="news-contet">
+        <div class="news-title">
+            ${item.title}
+        </div>
+        <div class="news-description">
+            ${item.description || item.content || ""}
+        </div>
+        <a href="${item.url}" target="_blank" class="view-button">Read More</a>
+    </div>;
+    `;
+    container.appendChild(card);
+  }
+};
+
+const getNews = async () => {
+  container.innerHTML = "";
+  let response = await fetch(requestURL);
+  if (!response.ok) {
+    alert("Data unavailable at the moment. Please try again later");
+    return false;
+  }
+  let data = await response.json();
+  generateUI(data.articles);
+};
+
+const init = () => {
+  optionsContainer.innerHTML = "";
+  getNews();
+  createOptions();
+};
+
+window.onload = () => {
+  requestURL = `https://newsapi.org/v2/top-headlines?country=${country}&category=general&apiKey=${apiKey}`;
+  init();
+};
